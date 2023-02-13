80507f267bbc562647c2af2e3809174e9a64e247
Mark Shivers <shivers.mark@gmail.com>
2023-02-13 16:00:33 -0700

    Knight Moves -Using Breadth First Search to find the shortest path, rather than a geometric approach

--- src/main/kotlin/ChessObjects.kt
+++ src/main/kotlin/ChessObjects.kt
@@ -1,12 +1,21 @@
-import KnightMoves.*
+import KnightMove.*
+import Point.Companion.INVALID_POINT
 import java.lang.RuntimeException
 import kotlin.math.absoluteValue
 import kotlin.math.sqrt
 
 data class ChessBoard(
-    val length: Int,
+    val height: Int,
     val width: Int,
-)
+) {
+    fun containsPoint(point: Point): Boolean {
+        if(height == -1 && width == -1) {
+            return (point.x >= 0) && (point.y >= 0)
+        } else{
+            return (point.x >= 0) && (point.x < width) && (point.y >= 0) && (point.y < height)
+        }
+    }
+}
 
 data class Point(
     val x: Int,
@@ -29,18 +38,24 @@ data class Point(
         return sqrt(y * y.toDouble() + x * x)
     }
 
+    fun findDistanceSquared(otherPoint: Point): Int {
+        return x * otherPoint.x + y * otherPoint.y
+    }
+
+    companion object {
+        val INVALID_POINT = Point(-1, -1)
+    }
+
 
 }
 
 class Knight(
     private val startingPoint: Point,
     private val destinationPoint: Point,
+    private val chessBoard: ChessBoard,
 ) {
+    private val path = mutableListOf<Point>()
     private var currentPoint = startingPoint
-
-    fun availableMoves(): List<Point> {
-        return availableDirections.values.map { it.add(currentPoint) }
-    }
 
     private fun findNextGeneralDirectionMove(currentPoint1: Point): Point {
         val directionOfDestination = currentPoint1.findDirection(destinationPoint)
@@ -49,48 +64,57 @@ class Knight(
             ?: throw RuntimeException("Unable to find next move")
     }
 
-    private fun dialInPosition(): List<Point> {
-        val nextMoves = mutableListOf<Point>()
-        //Direction should now be something like (-1,1), (1,0) but always just 1s and 0s
-        //TODO take into account the edge of the board
-        val nextDirections = when (destinationPoint.findDirection(currentPoint)) {
-            Point(0, 1) -> listOf(ONE_O_CLOCK, FOUR_O_CLOCK)
-            Point(1, 1) -> listOf(FIVE_O_CLOCK)
-            Point(1, 0) -> listOf(FOUR_O_CLOCK, SEVEN_O_CLOCK)
-            Point(1, -1) -> listOf(SEVEN_O_CLOCK)
-            Point(0, -1) -> listOf(FIVE_O_CLOCK, TWO_O_CLOCK)
-            Point(-1, -1) -> listOf(FOUR_O_CLOCK)
-            Point(-1, 0) -> listOf(EIGHT_O_CLOCK, FOUR_O_CLOCK)
-            Point(-1, 1) -> listOf(SEVEN_O_CLOCK)
-            else -> throw RuntimeException("Unable to determine next move")
+    fun findShortestPath(): PathNode {
+        val paths = mutableListOf<PathNode>()
+        paths.add(PathNode(startingPoint))
+        val visited = mutableSetOf<Point>()
+        while (paths.isNotEmpty()) {
+            val currentPathNode = paths.first()
+            if (currentPathNode.point == destinationPoint) {
+                return currentPathNode
+            }
+            paths.removeFirst()
+            KnightMove.values().forEach {
+                val nextPoint = currentPathNode.point.add(it.direction)
+                if (chessBoard.containsPoint(nextPoint)){
+                    if(!visited.contains(nextPoint)) {
+                        paths.add(PathNode(nextPoint, currentPathNode))
+                        visited.add(nextPoint)
+                    }
+                }
+            }
         }
-        nextDirections.map { it.point }.forEach {
-            currentPoint = currentPoint.add(it)
-            nextMoves.add(currentPoint)
-        }
-        currentPoint = destinationPoint
-        nextMoves.add(destinationPoint)
-        return nextMoves
+        return PathNode(Point(-1, -1))
+
     }
 
     fun findPath(): List<Point> {
-        val path = mutableListOf<Point>()
+
         while (currentPoint != destinationPoint) {
 //            println("currentPoint: $currentPoint")
-            path.add(currentPoint)
             if ((destinationPoint.x - currentPoint.x).absoluteValue < 2 && (destinationPoint.y - currentPoint.y) < 2) {
                 println("Got Close! current: $currentPoint, destination: $destinationPoint")
-                path.addAll(dialInPosition())
+//                path.addAll(dialInPosition(getPreviousSpace(path)))
                 break
             }
             currentPoint = findNextGeneralDirectionMove(currentPoint)
             if (path.size > 1 && currentPoint == path[path.size - 2]) {
                 println("Next move equals previous move")
+                path.add(INVALID_POINT)
                 break
             }
+            path.add(currentPoint)
         }
         return path
     }
+
+//    private fun getPreviousSpace(path: MutableList<Point>): Point {
+//        if (path.size > 1) {
+//            return path[path.size - 2]
+//        } else {
+//            return path[0]
+//        }
+//    }
 
     companion object {
         private val availableDirections = mapOf(
@@ -108,7 +132,7 @@ class Knight(
 }
 
 //Knight moves broken down into positions around an analog clock
-enum class KnightMoves(val point: Point) {
+enum class KnightMove(val direction: Point) {
     ONE_O_CLOCK(Point(1, 2)),
     TWO_O_CLOCK(Point(2, 1)),
     FOUR_O_CLOCK(Point(2, -1)),
@@ -117,4 +141,60 @@ enum class KnightMoves(val point: Point) {
     EIGHT_O_CLOCK(Point(-2, -1)),
     TEN_O_CLOCK(Point(-2, 1)),
     ELEVEN_O_CLOCK(Point(-1, 2)),
+}
+
+data class PathNode(
+    val point: Point,
+    val previousPathNode: PathNode? = null,
+) {
+    fun alreadyVisited(point: Point): Boolean {
+        return this.point == point || (previousPathNode?.alreadyVisited(point) == true)
+    }
+
+    fun didNotAlreadyVisit(point: Point) = !alreadyVisited(point)
+
+    fun length(): Int {
+        return 1 + (previousPathNode?.length() ?: 0)
+    }
+
+    fun toList(): List<Point> {
+        var currentPathNode = this
+        val pathList = mutableListOf(currentPathNode.point)
+
+        while(currentPathNode.previousPathNode != null) {
+            currentPathNode.previousPathNode?.let {
+                currentPathNode = it
+                pathList.add(currentPathNode.point)
+            }
+        }
+        return pathList.reversed()
+    }
+}
+
+fun doNOthing(directionToDestination: Point) = when (directionToDestination) {
+    Point(0, 1) -> Pair(
+        listOf(ONE_O_CLOCK, FIVE_O_CLOCK),
+        listOf(ELEVEN_O_CLOCK, SEVEN_O_CLOCK)
+    )
+
+    Point(1, 1) -> Pair(listOf(FIVE_O_CLOCK), listOf(TEN_O_CLOCK))
+    Point(1, 0) -> Pair(
+        listOf(FOUR_O_CLOCK, SEVEN_O_CLOCK),
+        listOf(TWO_O_CLOCK, TEN_O_CLOCK)
+    )
+
+    Point(1, -1) -> Pair(listOf(SEVEN_O_CLOCK), listOf(ONE_O_CLOCK))
+    Point(0, -1) -> Pair(
+        listOf(FIVE_O_CLOCK, TWO_O_CLOCK),
+        listOf(FOUR_O_CLOCK, ONE_O_CLOCK)
+    )
+
+    Point(-1, -1) -> Pair(listOf(FOUR_O_CLOCK), listOf(ELEVEN_O_CLOCK))
+    Point(-1, 0) -> Pair(
+        listOf(EIGHT_O_CLOCK, FOUR_O_CLOCK),
+        listOf(TEN_O_CLOCK, FIVE_O_CLOCK)
+    )
+
+    Point(-1, 1) -> Pair(listOf(SEVEN_O_CLOCK), listOf(TWO_O_CLOCK))
+    else -> Pair(emptyList(), emptyList())
 }
--- src/test/kotlin/KnightTest.kt
+++ src/test/kotlin/KnightTest.kt
@@ -1,31 +1,65 @@
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
 class KnightTest {
 
-    @Test
-    fun `Get Knight Close to Destination`() {
-        val knight = Knight(Point(20,20), Point(0,0))
-        val path = knight.findPath()
-        println("Arrived at destination in ${path.size} moves, $path")
-    }
 
     @ParameterizedTest
-    @MethodSource("destinations")
-    fun `Test multiple paths`(destination: Point) {
-        val knight = Knight(Point(0,0), destination)
-        val path = knight.findPath()
-        println("Arrived at destination in ${path.size} moves, $path")
+    @MethodSource("startEndPointsBoardDimensions")
+    fun `Test multiple paths`(startingSpace: Point, destination: Point, chessBoard: ChessBoard) {
+        val knight = Knight(startingSpace, destination, chessBoard)
+        val path = knight.findShortestPath()
+        println("Arrived at destination in ${path.length()} moves, $path")
+        assertFalse {  path.toList().any { it.x < 0 || it.y < 0 || it.x >= chessBoard.width || it.y >= chessBoard.height }}
+        assertEquals(destination, path.point)
     }
 
-    companion object{
+    @Test
+    fun `Already Visited Test`() {
+        val pathNode = PathNode(Point(44,33), PathNode(Point(33,56),PathNode(Point(1,2))))
+        assertTrue { pathNode.alreadyVisited(Point(1,2)) }
+    }
+
+//    @ParameterizedTest
+//    @MethodSource("invalidStartEndPointsBoardDimensions")
+//    fun `Test multiple invalid paths`(startingSpace: Point, destination: Point, xDimension: Int, yDimension: Int) {
+//        val knight = Knight(startingSpace, destination)
+//        val path = knight.findPath()
+//        println("Arrived at destination in ${path.size} moves, $path")
+//        assertEquals(Point(-1,-1), path.last())
+//    }
+
+    companion object {
         @JvmStatic
-        fun destinations() = listOf(
-            Point(45,20),
-            Point(5,20),
-            Point(5,4),
-            Point(45,20),
+        fun startEndPointsBoardDimensions() = listOf(
+            Arguments.of(Point(0, 0), Point(45, 20), ChessBoard(100, 100)),
+            Arguments.of(Point(0, 0), Point(5, 20), ChessBoard(100, 100)),
+            Arguments.of(Point(0, 0), Point(5, 4), ChessBoard(100, 100)),
+            Arguments.of(Point(20, 20), Point(5, 4), ChessBoard(100, 100)),
+            Arguments.of(Point(0, 0), Point(45, 20), ChessBoard(100, 100)),
+            Arguments.of(Point(0, 0), Point(99, 99), ChessBoard(100, 100)),
+            Arguments.of(Point(0, 0), Point(98, 99), ChessBoard(100, 100)),
+            Arguments.of(Point(0, 0), Point(97, 99), ChessBoard(100, 100)),
+            Arguments.of(Point(0, 0), Point(96, 99), ChessBoard(100, 100)),
+            Arguments.of(Point(0, 0), Point(96, 1), ChessBoard(100, 100)),
+            Arguments.of(Point(0, 0), Point(99, 1), ChessBoard(100, 100)),
+            Arguments.of(Point(0, 0), Point(99, 0), ChessBoard(100, 100)),
+            Arguments.of(Point(0, 0), Point(0, 99), ChessBoard(100, 100)),
+            Arguments.of(Point(24, 31), Point(0, 99), ChessBoard(100, 100)),
+        )
+
+        @JvmStatic
+        fun invalidStartEndPointsBoardDimensions() = listOf(
+            Arguments.of(Point(0, 0), Point(45, 20), 2, 100),
+            Arguments.of(Point(0, 0), Point(5, 20), 2, 100),
+            Arguments.of(Point(0, 0), Point(5, 4), 2, 100),
+            Arguments.of(Point(0, 0), Point(5, 4), 2, 100),
+            Arguments.of(Point(0, 0), Point(45, 20), 2, 100),
         )
     }
 }
